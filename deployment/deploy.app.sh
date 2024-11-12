acbece721b3225a6e4664e0263775aa2abf2836c
Azamat Holmeer <azamat.holmeer@gmail.com>
2024-11-12 16:58:27 +0500

    separate deploy job

--- deployment/deploy.app.sh
+++ deployment/deploy.app.sh
@@ -9,17 +9,5 @@ source ./deployment/config.sh
 # Docker image names
 export APP_DOCKER_IMAGE="${DOCKER_REGISTRY}/app:${BUILD_VERSION}"
 
-# Build dist
-npx nx run app:build
-
-## build & push 'app' docker image
-docker build \
-  --platform linux/amd64 \
-  --tag ${APP_DOCKER_IMAGE} \
-  --file ./deployment/docker/app.Dockerfile .
-docker push ${APP_DOCKER_IMAGE}
-
-# Deploy services & jobs to Google Cloud
-
 ## Render and deploy app service
-#gcloud run services replace $(./deployment/utils/render.sh deployment/services/app.service.yaml) --region=${REGION} --project=${PROJECT_ID}
+gcloud run services replace $(./deployment/utils/render.sh deployment/services/app.service.yaml) --region=${REGION} --project=${PROJECT_ID}
